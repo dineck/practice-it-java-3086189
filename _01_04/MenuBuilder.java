ef1514145512644c249bbafbcbe43b99c1f45a02
Gurita Dinu <dineckg@gmail.com>
2024-10-06 22:00:19 +0300

    done

--- _01_04/MenuBuilder.java
+++ _01_04/MenuBuilder.java
@@ -13,25 +13,30 @@ public class MenuBuilder {
     System.out.println(menuTitle);
 
     // Create a variable called menu of type ArrayList.
-    ArrayList[] menu;
+    ArrayList<MenuItem> menu=new ArrayList<>();
 
     // Create a variable called starter of type MenuItem and pass in the name of
     // your favourite starter.
-    MenuItem starter=new MenuItem("bors");
+    MenuItem starter=new MenuItem("Calamari");
 
     // Add the starter variable to the ArrayList called menu.
-    menu[0]=starter.();
+    menu.add(starter);
 
     // Create a variable called mainCourse of type MenuItem and pass in the name of
     // your favourite main course.
+    MenuItem mainCourse=new MenuItem("Lasagne");
 
     // Add the mainCourse variable to the ArrayList called menu.
+    menu.add(mainCourse);
 
     // Create a variable called dessert of type MenuItem and pass in the name of
     // your favourite dessert.
+    MenuItem dessert=new MenuItem("Banoffee Pie");
 
     // Add the dessert variable to the ArrayList called menu.
+    menu.add(dessert);
 
     // Print the menu variable to the console.
+    System.out.println(menu);
   }
 }
